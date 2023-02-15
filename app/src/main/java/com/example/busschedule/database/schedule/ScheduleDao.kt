adc5c9d03b8eb118387997ac2eeabc17c9606d16
elv-sabrina <sabrina.ahmd@eduge.ch>
2023-02-15 11:44:50 +0100

    définir view model + étape 7

--- app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -2,14 +2,15 @@ package com.example.busschedule.database.schedule
 
 import androidx.room.Dao
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
-class ScheduleDao {
-    @Dao
-    interface ScheduleDao {
-        @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
-        fun getAll(): List<Schedule>
+@Dao
+interface ScheduleDao {
 
-        @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
-        fun getByStopName(stopName: String): List<Schedule>
-    }
+    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    fun getAll(): Flow<List<Schedule>>
+
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
+    fun getByStopName(stopName: String): Flow<List<Schedule>>
+
 }
